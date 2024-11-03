27946f7da5050bac02155c8eac0ef111097cd9b6
Ohioooooo <157407505+Ohioooooo@users.noreply.github.com>
2024-11-03 15:59:46 -0700

    Update code_12.py

--- code_12.py
+++ code_12.py
@@ -8,9 +8,14 @@ def get_fibonacci_number(position):
 
 def get_fibonacci_number_sequence(number):
     
-     = []
+    stored_numbers = []
+    for i in range(1, number + 1):  # Loop through numbers. 
+        print(get_fibonacci_number(i))  # Print the fibonacci numbers in the order depending on the number input.
+
 
 
 
 if __name__ == "__main__":
-    print(get_fibonacci_number(6))
+    print(get_fibonacci_number(6))  # this prints number in the sequence in the position given by user.
+
+    get_fibonacci_number_sequence(9)  # this prints the sequence 1 by 1. 
